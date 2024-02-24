79ac4d20366417772b93db9af9606621d9a14ac5
infiPa <oyunh3018@gmail.com>
2024-02-24 17:17:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+function App() {
+  return (
+    <>
+      <div>test</div>
+      <img src="/logo192.png" alt="" />
+      <img src={logo} alt="" />
+    </>
+  );
+}
+
+export default App; 
